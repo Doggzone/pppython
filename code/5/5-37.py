d014aff0feaba85411b052c4e140b0ba7670f2ab
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:16:22 +0900

    Create 5-37.py

---
+++ code/5/5-37.py
@@ -0,0 +1,14 @@
+def vowel_numbering(word):
+    number =
+    newword =
+    for c in word:
+        if c in ['a','e','i','o','u','A','E','I','O','U']:
+            newword =
+            number =
+        else:
+            newword =
+    return newword
+
+# Test code
+print(vowel_numbering('Massachussettes')) # 'M1ss2ch3ss4tt5s'
+
