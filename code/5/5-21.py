5a0dd79375f1735c9d70faa0d4598164ebe6ea1a
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:15:45 +0900

    Create 5-21.py

---
+++ code/5/5-21.py
@@ -0,0 +1,7 @@
+def insertion_sort(xs):
+    def loop(xs,ss):
+        if xs != []:
+            pass # Write your code here.
+        else:
+            pass # Write your code here.
+    return loop(xs,[])
