fcc14e353b54b83d813e68f3706e8d6340bacd49
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:16:47 +0900

    Create 5-47.py

---
+++ code/5/5-47.py
@@ -0,0 +1,13 @@
+def bin2dec(bin):
+    length = len(bin)
+    dec = 0
+    for i in range(length):
+        pass # Write your code here.
+    return dec
+
+# Test code
+print(bin2dec("0"))      # 0
+print(bin2dec("1"))      # 1
+print(bin2dec("110"))    # 6
+print(bin2dec("10011"))  # 19
+print(bin2dec("101010")) # 42
