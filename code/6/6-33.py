4d8df691811382c4130257f11205d91cc7e0cad0
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:18:06 +0900

    Create 6-33.py

---
+++ code/6/6-33.py
@@ -0,0 +1,16 @@
+def longest_streak1(s):
+	pass # write your code here.
+
+# Test code
+print(longest_streak1("")) 
+# None
+print(longest_streak1("5")) 
+# ('5', 1, 0)
+print(longest_streak1("06479019955907200041185008780528384811265678111671")) 
+# ('0', 3, 15)
+print(longest_streak1("49715114250863455559013207228395154984882560834674")) 
+# ('5', 4, 15)
+print(longest_streak1("79083787262159815638834042282485195270836937488097")) 
+# ('8', 2, 19)
+print(longest_streak1("36888653851748777011129000999371447120618209984726")) 
+# ('8', 3, 2)
