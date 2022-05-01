9e29b0fc6519d4ddf191d56e189c1c1687423503
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:44 +0900

    Create 6-25.py

---
+++ code/6/6-25.py
@@ -0,0 +1,8 @@
+def search_widest_gap(ss):
+	pass # write your code here.
+
+# Test code
+print(search_widest_gap([]))                     # (None, 0)
+print(search_widest_gap([3]))                    # (0, 0)
+print(search_widest_gap([3,5,8,20,22]))          # (2, 12)
+print(search_widest_gap([3,5,8,20,22,34,37,40])) # (2, 12)
