7536c5fb7cba78467447b66b24cc8eb392a9bc21
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:15:57 +0900

    Create 5-26.py

---
+++ code/5/5-26.py
@@ -0,0 +1,12 @@
+def merge(left,right):
+    def loop(left,right,ss):
+        if not (left == [] or right == []):
+            if left[0] <= right[0]:
+                pass # Write your code here.
+               
+            else:
+                pass # Write your code here.
+                
+        else:
+            return ss + left + right
+    return loop(left,right,[])
