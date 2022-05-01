a21b1f7a29776485048db3cbc876de4c436f871b
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:19:49 +0900

    Create 8-23.py

---
+++ code/8/8-23.py
@@ -0,0 +1,12 @@
+def find_all_pairs(ns,n):
+    pairs = []
+    pass # Write your nested for-loop here.
+    return pairs
+
+# Test code
+print(find_all_pairs([4, 6, 5, 3, 7, 6, 2, 1, 3, 2],10))
+# [(0, 1), (0, 5), (3, 4), (4, 8)]
+print(find_all_pairs([4, 6, 5, 3, 7, 6, 2, 1, 3, 2],5))
+# [(0, 7), (3, 6), (3, 9), (6, 8), (8, 9)]
+print(find_all_pairs([4, 6, 5, 3, 7, 6, 2, 1, 3, 2],7))
+# [(0, 3), (0, 8), (1, 7), (2, 6), (2, 9), (5, 7)]
