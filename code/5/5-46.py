834da450ac6758b9578a9c3b7e4e5e4943d0358b
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:16:45 +0900

    Create 5-46.py

---
+++ code/5/5-46.py
@@ -0,0 +1,8 @@
+def substrings(s):
+subs = ['']
+pass  # Write your for-loop code here.
+return subs
+
+# Test code
+print(substrings("ERICA")) # ['', 'E', 'R', 'I', 'C', 'A', 'ER', 'RI', 'IC', 'CA', 'ERI', 'RIC', 'ICA', 'ERIC', 'RICA', 'ERICA']
+print(substrings("")) # ['']
