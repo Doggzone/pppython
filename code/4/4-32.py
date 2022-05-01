d254dac5fb81f591a03124f3bde8f5e9699810d7
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:13 +0900

    Create 4-32.py

---
+++ code/4/4-32.py
@@ -0,0 +1,11 @@
+def russian_mult(m,n):
+    def loop(m,n):
+        if n > 1:
+            pass # Fill your code in this space.
+
+        else: # n == 1
+            pass # Fill your code in this line.
+    if n > 0:
+        return loop(m,n)
+    else:
+        return 0
