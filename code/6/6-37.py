ae99575116cc1cce62813706489a3890b83cf532
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:18:14 +0900

    Create 6-37.py

---
+++ code/6/6-37.py
@@ -0,0 +1,8 @@
+def primes(n):
+	pass # write your code here.
+
+# Test code
+print(primes(0)) # []
+print(primes(1)) # [2]
+print(primes(5)) # [2, 3, 5, 7, 11]
+print(primes(10)) # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
