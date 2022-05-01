adb162c4a9a95b530a134fbb5b9b54c4a504f728
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:49 +0900

    Create 6-27.py

---
+++ code/6/6-27.py
@@ -0,0 +1,11 @@
+def ascending(ns):
+	pass # write your code here.
+
+# Test code
+print(ascending([1,3]))                # True
+print(ascending([2, 3, 6, 8, 12, 17])) # True
+print(ascending([]))                   # False
+print(ascending([2]))                  # False
+print(ascending([3, 3, 3, 3, 3]))      # False
+print(ascending([1, 2, 2, 3]))         # False
+print(ascending([2, 3, 1]))            # False
