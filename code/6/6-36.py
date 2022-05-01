6328581b87043092ac27efd20e417bbc69972695
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:18:12 +0900

    Create 6-36.py

---
+++ code/6/6-36.py
@@ -0,0 +1,10 @@
+def all_primes_less_than(n):
+	pass # write your code here.
+
+# Test code
+print(all_primes_less_than(2)) # []
+print(all_primes_less_than(3)) # [2]
+print(all_primes_less_than(5)) # [2, 3]
+print(all_primes_less_than(10)) # [2, 3, 5, 7]
+print(all_primes_less_than(20)) # [2, 3, 5, 7, 11, 13, 17, 19]
+print(all_primes_less_than(30)) # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
