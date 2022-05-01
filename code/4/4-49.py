15e7a63d0897b54bd6d9fc48c87af55ed30068a0
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:57 +0900

    Create 4-49.py

---
+++ code/4/4-49.py
@@ -0,0 +1,19 @@
+def sigma2(n):
+	pass # Write your code here.
+
+# Test code
+print(sigma2(13)) # 91
+print(sigma2(12)) # 78
+print(sigma2(11)) # 66
+print(sigma2(10)) # 55
+print(sigma2(9))  # 45
+print(sigma2(8))  # 36
+print(sigma2(7))  # 28
+print(sigma2(6))  # 21
+print(sigma2(5))  # 15
+print(sigma2(4))  # 10
+print(sigma2(3))  # 6
+print(sigma2(2))  # 3
+print(sigma2(1))  # 1
+print(sigma2(47733))    # 1139243511
+print(sigma2(37387282)) # 698904446367403
