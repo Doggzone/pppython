196f5dadb3817e70f794fbd97039b7e5dd7cf3d4
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:40 +0900

    Create 4-41.py

---
+++ code/4/4-41.py
@@ -0,0 +1,10 @@
+def square(n):
+	pass # Write your code here.
+
+# Test code
+print(square(0))  # 0
+print(square(1))  # 1
+print(square(-2)) # 4
+print(square(3))  # 9
+print(square(-4)) # 16
+print(square(5))  # 25
