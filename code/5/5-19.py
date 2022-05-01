89311fe5db0b6fdb9883a036519df41650c85640
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:15:38 +0900

    Create 5-19.py

---
+++ code/5/5-19.py
@@ -0,0 +1,11 @@
+def insert(x,ss):
+    left = []
+    while ss != []:
+        if x <= ss[0]:
+            pass # Write your code here.
+
+        else:
+            pass # Write your code here.
+
+    left.append(x)
+    return left
