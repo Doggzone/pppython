a5df1cc2bdf9aa2d1e893683678736f2c6deb13d
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:08:13 +0900

    Create 2-19.py

---
+++ code/2/2-19.py
@@ -0,0 +1,13 @@
+def complement_nine(n):
+    return None # Write your expression here.
+    
+# Test code
+print(complement_nine(0))    # 9
+print(complement_nine(9))    # 0
+print(complement_nine(4))    # 5
+print(complement_nine(18))   # 81
+print(complement_nine(40))   # 59
+print(complement_nine(307))  # 692
+print(complement_nine(9142)) # 857
+print(complement_nine(9965)) # 34
+print(complement_nine(9999)) # 0
