14aff5c8f462e06b61e1a89a923c47a6b67d2ec2
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:16:08 +0900

    Create 5-30.py

---
+++ code/5/5-30.py
@@ -0,0 +1,11 @@
+def partition(pivot,xs):
+    def loop(xs,left,right):
+        if xs != []:
+            pass # Write your code here.
+
+
+
+
+        else:
+            return left,right
+    return loop(xs,[],[]) 
