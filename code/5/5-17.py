4dd80fe0a1692005660e6e58052cee8a07cd439f
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:15:34 +0900

    Create 5-17.py

---
+++ code/5/5-17.py
@@ -0,0 +1,8 @@
+def insert(x,ss):
+    if ss != []:
+        if x <= ss[0]:
+            pass # Write your code here.
+        else:
+            pass # Write your code here.
+    else:
+        return [x]
