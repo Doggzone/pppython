7670e99961ca39d1228611fdcac5896b6437d4fd
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:35 +0900

    Create 6-21.py

---
+++ code/6/6-21.py
@@ -0,0 +1,12 @@
+def rankith(ns,i):
+	pass # Write your code here.
+
+# Test code
+print(rankith([5,2,3,6,4,3,7,5,8,2],1)) # 8
+print(rankith([5,2,3,6,4,3,7,5,8,2],2)) # 7
+print(rankith([5,2,3,6,4,3,7,5,8,2],4)) # 5
+print(rankith([5,2,3,6,4,3,7,5,8,2],5)) # 5
+print(rankith([5,2,3,6,4,3,7,5,8,2],6)) # 4
+print(rankith([5,2],2))                 # 2
+print(rankith([5],1))                   # 5
+print(rankith([],1))                    # None
