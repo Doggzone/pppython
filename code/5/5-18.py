459fc7142aed3aabff15c7731cecedfa90126355
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:15:36 +0900

    Create 5-18.py

---
+++ code/5/5-18.py
@@ -0,0 +1,13 @@
+def insert(x,ss):
+    def loop(ss,left):
+        if ss != []:
+            if x <= ss[0]:
+                pass # Write your code here.
+
+            else:
+                pass # Write your code here.
+
+        else:
+            left.append(x)
+            return left
+    return loop(ss,[])
