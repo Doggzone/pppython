4f8c76b4045d31e7f46e14085dcc410b088b75fa
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:18:00 +0900

    Create 6-31.py

---
+++ code/6/6-31.py
@@ -0,0 +1,7 @@
+def longest_steepest_ascending_sublist(ns):
+	pass # write your code here.
+
+# Test code
+print(longest_steepest_ascending_sublist([1,5,3,4,8,2,3,5])) # [3, 4, 8]
+print(longest_steepest_ascending_sublist([]))     # []
+print(longest_steepest_ascending_sublist(sample)) # [7, 23, 34, 67, 77]
