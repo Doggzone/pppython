e083e87d39c6ab9f8583a7fb2c30fef9eae35562
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:08:11 +0900

    Create 2-18.py

---
+++ code/2/2-18.py
@@ -0,0 +1,6 @@
+def fahrenheit2celsius(f):
+    return None # Write your expression here.
+
+# # Test code
+# print(fahrenheit2celsius(67)) # 19.444444444444443
+# print(round(fahrenheit2celsius(67),1)) # 19.4
