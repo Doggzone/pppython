27137a60f892750c0c6cbec70a536972487fc63f
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:18:17 +0900

    Create 6-38.py

---
+++ code/6/6-38.py
@@ -0,0 +1,8 @@
+def twin_primes(n):
+	pass # write your code here.
+
+# Test code
+print(twin_primes(0)) # []
+print(twin_primes(1)) # [(3, 5)]
+print(twin_primes(5)) # [(3, 5), (5, 7), (11, 13), (17, 19), (29, 31)]
+print(twin_primes(10)) # [(3, 5), (5, 7), (11, 13), (17, 19), (29, 31), (41, 43), (59, 61), (71, 73), (101, 103), (107, 109)]
