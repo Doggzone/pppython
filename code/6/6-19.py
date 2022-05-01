89496202ed4219591960d32f7bb9edb8ed4d073e
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:28 +0900

    Create 6-19.py

---
+++ code/6/6-19.py
@@ -0,0 +1,9 @@
+def find_all_count(filename, x):
+    pass # Write your code here.
+
+# Test code
+# find_all_count('article.txt','computer')     # 6 time(s).
+# find_all_count('article.txt','Whole Earth')  # 2 time(s).
+# find_all_count('article.txt','Apple')        # 9 time(s).
+# find_all_count('article.txt','commencement') # 1 time(s).
+# find_all_count('article.txt','apple')        # 0 time(s).
