1df301e95a4f4f6c9ec0007611ee9b9df292f1da
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:08:19 +0900

    Create 2-20.py

---
+++ code/2/2-20.py
@@ -0,0 +1,6 @@
+def celsius2fahrenheit(c):
+    return None # Write your expression here.
+    
+# Test code
+print(celsius2fahrenheit(19.4)) # 66.92
+print(round(celsius2fahrenheit(19.4),1)) # 66.9
