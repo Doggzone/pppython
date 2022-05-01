5ec4528c9d69fff42358d459a0ea6f1072813444
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:20:15 +0900

    Create 9-10.py

---
+++ code/9/9-10.py
@@ -0,0 +1,6 @@
+def show_top5(members):
+    print("---")
+    sorted_members = None # Sort the number of chips in reverse order.
+    print("All-time Top 5 based on the number of chips earned")
+    # Show the elements of sorted_members[:5] in order. 
+    # Disregard the record if the number of chips is 0 or less
