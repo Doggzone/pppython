c0a8a0ea3ce372674171f96fc9eb07003ffd2fb4
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:37 +0900

    Create 4-40.py

---
+++ code/4/4-40.py
@@ -0,0 +1,10 @@
+def trinum(n):
+	pass # Write your code here.
+
+# Test code
+print(trinum(1))  # 1
+print(trinum(3))  # 6
+print(trinum(6))  # 21
+print(trinum(11)) # 66
+print(trinum(0))  # 0
+print(trinum(-3)) # 0
