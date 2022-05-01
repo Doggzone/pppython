0677a5d3b3f816984bb5fff586321e005dc9a5f4
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:26 +0900

    Create 6-18.py

---
+++ code/6/6-18.py
@@ -0,0 +1,12 @@
+def find_all(filename, x):
+    pass # Write your code here.
+
+# Test code
+# find_all('article.txt','computer')
+# at 3269, 3357, 3601, 3725, 6209, 10975.
+# find_all('article.txt','Whole Earth')
+# at 10735, 11280.
+# find_all('article.txt','Apple')
+# at 4380, 4455, 4742, 5586, 5765, 6346, 6379, 6445, 6604.
+# find_all('article.txt','apple')
+# not found
