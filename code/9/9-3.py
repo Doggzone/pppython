100510f132b8534807de30b271fbb3ac670a5624
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:20:23 +0900

    Create 9-3.py

---
+++ code/9/9-3.py
@@ -0,0 +1,16 @@
+def count_score(cards):
+    score = 0
+    number_of_ace = 0
+    for card in cards:
+        rank = card[1]
+        # accumulate score (counts A as 11)
+
+
+
+
+
+
+    # adjust score if score is over 21 and there is A
+    # there may be two or more A's
+    
+    return score
