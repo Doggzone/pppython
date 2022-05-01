c71b075d76d7342e193de8f32389369dc3a5c18c
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:20:13 +0900

    Create 9-1.py

---
+++ code/9/9-1.py
@@ -0,0 +1,11 @@
+import random
+def fresh_deck():
+    suits = {"Spade", "Heart", "Diamond", "Club"}
+    ranks = {2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"}
+    deck = []
+    # Write your nested for-loop creating 52 cards in deck.
+
+
+   
+    # shuffle deck using random.shuffle function
+    return deck
