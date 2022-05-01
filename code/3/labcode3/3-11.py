651502b8ea98a8a1265bd2b4124958d9a518d518
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:10:48 +0900

    Create 3-11.py

---
+++ code/3/labcode3/3-11.py
@@ -0,0 +1,16 @@
+from math import pi
+
+def area_circle(radius, n):
+    if radius > 0:
+        area = pi * radius ** 2
+        return round(area, n)
+    else:
+        return 0.0
+
+print("Circle Area Calculator")
+while True:
+    r = input("Radius? ")
+    p = input("Precision? ")
+    area = area_circle(int(r),int(p))
+    print("The area of a circle with radius", r, "is", area, ".")
+print("Please come back agan.")
