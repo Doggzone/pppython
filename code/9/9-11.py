0fad82bd7a4255c1e81517869b3982c9346136ef
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:20:17 +0900

    Create 9-11.py

---
+++ code/9/9-11.py
@@ -0,0 +1,13 @@
+def sparse(ns):
+    dic = {}
+    index = 0
+    pass # Write your for-loop here.
+
+
+
+    return dic
+
+# Test code
+print(sparse([])) # {}
+print(sparse([0,0,3,0,0,0,0,0,0,7,0,0])) # {2: 3, 9: 7}
+print(sparse([1,0,2,0,0,0,9,0,0])) # {0: 1, 2: 2, 6: 9}
