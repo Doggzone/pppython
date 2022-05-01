94b6b85b433e55996656e01b774386b2b84ac31d
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:47 +0900

    Create 4-44.py

---
+++ code/4/4-44.py
@@ -0,0 +1,8 @@
+def add_range(start,stop,step):
+	pass # Write your code here.
+
+# Test code
+print(add_range(1,10,1)) # 45
+print(add_range(1,10,2)) # 25
+print(add_range(1,10,3)) # 12
+print(add_range(-5,6,2)) # 0
