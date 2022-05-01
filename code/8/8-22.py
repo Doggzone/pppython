3bb690ec203272ee3988ef688fac6f87513f0020
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:19:47 +0900

    Create 8-22.py

---
+++ code/8/8-22.py
@@ -0,0 +1,10 @@
+def blast(ns):
+    bs = []
+    pass # Write your for-loop here.
+    return bs
+
+# Test code
+print(blast([]))       # []
+print(blast([1,2,4]))  # [1,2,2,4,4,4,4]
+print(blast([3,5]))    # [3,3,3,5,5,5,5,5]
+print(blast([2,-3,3])) # [2,2,3,3,3]
