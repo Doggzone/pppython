26c2cfaaa39848bba3bc11d0a90f353c565e1f95
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:49:04 +0900

    Create 3-29.py

---
+++ code/3/3-29.py
@@ -0,0 +1,8 @@
+def addtime(time1,time2):
+    pass # Write your code here.
+
+# Test code
+print(addtime("0:14:57","0:25:02")) # "0:39:59"
+print(addtime("0:14:57","0:51:01")) # "1:05:58"
+print(addtime("0:14:57","0:51:18")) # "1:06:15"
+print(addtime("9:43:57","8:51:45")) # "18:35:42"
