36f380856439b75a896199f6965ba826254d366c
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:09:42 +0900

    Create 3-13.py

---
+++ code/3/3-13.py
@@ -0,0 +1,12 @@
+print("Score Average Calculator")
+number = input("How many classes? ")
+total = 0
+count = 0
+while None: # Write your loop condition
+    pass # Get and accumulate scores
+
+
+if count > 0:
+    pass # Compute and print the average
+else:
+    print("Your average score = 0.0")
