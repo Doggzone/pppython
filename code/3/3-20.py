8bf9f123fe0e7432e0ec6efd5479acf5fe8b0c24
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:48:44 +0900

    Create 3-20.py

---
+++ code/3/3-20.py
@@ -0,0 +1,9 @@
+def median(x,y,z):
+	pass # Write your code here.
+
+# Test code
+print(median(1,3,5)) # 3
+print(median(3,5,1)) # 3
+print(median(3,3,3)) # 3
+print(median(3,5,3)) # 3
+print(median(3,5,5)) # 5
