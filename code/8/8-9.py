d1eb064d5df625b75ec92dd80e73434384f77ca1
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:20:10 +0900

    Create 8-9.py

---
+++ code/8/8-9.py
@@ -0,0 +1,7 @@
+def transpose(board):
+    size = len(board)
+    transposed = [[] for _ in range(size)]
+    for row in board:
+        for i in range(size):
+            pass # Write your code here.
+    return transposed
