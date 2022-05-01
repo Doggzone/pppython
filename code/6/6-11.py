9d4a5edfda252c4dbacbdb25194718aa16e5b120
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:11 +0900

    Create 6-11.py

---
+++ code/6/6-11.py
@@ -0,0 +1,6 @@
+def bin_search_OX(ss,x):
+    mid = len(ss) // 2
+    return ss != [] and \
+           (x == ss[mid] or \
+                                                      or \
+                                           )
