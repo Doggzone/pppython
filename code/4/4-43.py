4d97355bffddd73594811c77d4f94878828748b2
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:14:44 +0900

    Create 4-43.py

---
+++ code/4/4-43.py
@@ -0,0 +1,10 @@
+def sum_of_num_over_next(n):
+	pass # Write your code here.
+
+# Test code
+print(sum_of_num_over_next(0))   # 0
+print(sum_of_num_over_next(1))   # 0.5
+print(sum_of_num_over_next(3))   # 1.9166666666666665
+print(sum_of_num_over_next(5))   # 3.5500000000000003
+print(sum_of_num_over_next(10))  # 7.980122655122655
+print(sum_of_num_over_next(100)) # 95.80272149226131
