c1a3a8eb2ecc97f6cbe9ad26ed74ca02bcc9abfd
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:17:40 +0900

    Create 6-23.py

---
+++ code/6/6-23.py
@@ -0,0 +1,15 @@
+def collect_dups(s):
+    singles = ""
+    duplicates = []
+    pass # Write your for-loop here.
+    return duplicates
+
+# Test code
+print(collect_dups("sophisticated"))
+# ['s', 'i', 't']
+print(collect_dups("I'm no angel."))
+# [' ', 'n']
+print(collect_dups("Stay Hungry. Stay Foolish."))
+# ['y', ' ', 'S', 't', 'a', 'o', '.']
+print(collect_dups("Your time is limited, so don't waste it living someone else's life."))
+# [' ', 'i', 'm', 't', 'e', 's', 'o', 'd', 'l', 'n', "'"]
