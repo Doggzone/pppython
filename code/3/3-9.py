2c4f50c8e31ab02bfb7a0810b6856fd39923afa4
Doh Kyung-Goo <doggzone@gmail.com>
2022-05-01 20:10:41 +0900

    Create 3-9.py

---
+++ code/3/3-9.py
@@ -0,0 +1,14 @@
+def smallest(x,y,z):
+    return None # Write your expression here.
+
+# Test code
+print(smallest(3,5,9)) # 3
+print(smallest(5,3,9)) # 3
+print(smallest(5,9,3)) # 3
+print(smallest(3,9,5)) # 3
+print(smallest(9,3,5)) # 3
+print(smallest(9,5,3)) # 3
+print(smallest(3,3,5)) # 3
+print(smallest(5,3,3)) # 3
+print(smallest(3,5,3)) # 3
+print(smallest(3,3,3)) # 3
